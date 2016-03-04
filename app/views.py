c24d67fca0a2de8e10a118a64690bcde4413b57e
Hoa Nguyen <emhoa@hotmail.com>
2016-03-04 10:52:09 -0500

    debugging

--- app/views.py
+++ app/views.py
@@ -17,65 +17,67 @@ from flask import request
 def index():
 	return render_template("bulkInsertGrades.html")
 
-@app.route('/test')
-def mytest():
-	return render_template("test.html")
-
 def get_timestamp(): 
 	return datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
 
 @app.route('/restaurants', methods=['POST'])
 def bulkInsert():
-	print "in bulk insert"
+
+	#Set up some constants
 	RESTAURANT_SOURCE_FILE="https://nycopendata.socrata.com/api/views/xx67-kt59/rows.csv?accessType=DOWNLOAD"
 	RESTAURANT_DEST_FILE="/tmp/" + "restaurantgrades" + datetime.datetime.now().strftime("%Y-%m-%d") + ".csv"
 	DOWNLOAD_DATA_TABLE="input_data"
 
+	#Download the restaurant data file from NYC site
+	print get_timestamp() + ": Starting download of source data file"
+	try:
+		r = requests.get(RESTAURANT_SOURCE_FILE)
+	except Exception as e:
+		print get_timestamp() + ": Unable to download file " + RESTAURANT_SOURCE_FILE
+		return render_template("bulkInsertGradesResults.html", outcome="failed with error: " + str(e))
 
+	print get_timestamp() + ": Downloaded " + RESTAURANT_SOURCE_FILE
+
+	#Open local destination data file for writing
+	try:
+		download_file = open(RESTAURANT_DEST_FILE, "wb")
+	except Exception as e:
+		print get_timestamp() + ": Unable to open " + RESTAURANT_DEST_FILE + " for writing."
+		return render_template("bulkInsertGradesResults.html", outcome="failed with error: " + str(e))
+
+
+	download_file.write(r.content)
+	download_file.close()
+
+	#Connect to postgres database
 	urlparse.uses_netloc.append("postgres")
 	url = urlparse.urlparse(os.environ["DATABASE_URL"])
 
 	try:
-#	conn = psycopg2.connect("dbname='restaurantgrades' user='postgres' host='localhost' password='postgres'")
 		conn = psycopg2.connect(database=url.path[1:], user=url.username, password=url.password, host=url.hostname, port=url.port)
-	except:
+	except Exception as e:
 		print "Unable to connect to the database"
-		return render_template("bulkInsertGrades.html")
+		return render_template("bulkInsertGradesResults.html", outcome="failed with error: " + str(e))
 
 
-	print get_timestamp() + ": Starting download of source data file"
-
-	try:
-		download_file = open(RESTAURANT_DEST_FILE, "wb")
-	except:
-		print get_timestamp() + ": Unable to open " + RESTAURANT_DEST_FILE + " for writing."
-		return render_template("bulkInsertGrades.html")
-
-	try:
-		r = requests.get(RESTAURANT_SOURCE_FILE)
-	except:
-		print get_timestamp() + ": Unable to download file " + RESTAURANT_SOURCE_FILE
-		return render_template("bulkInsertGrades.html")
-
-	print get_timestamp() + ": Downloaded " + RESTAURANT_SOURCE_FILE
-	download_file.write(r.content)
-	download_file.close()
-
+	#Open restaurant data file and peak at first line to find column names
 	print get_timestamp() + ": Opening file to find column names"
 	restaurant_file = open(RESTAURANT_DEST_FILE, "rb")
 
 	columns = restaurant_file.readline().split(",")
 	restaurant_file.close()
+
+	#Prepare three queries that will specify column names: drop, create and bulk copy
 	drop_table_query = """DROP table if exists """ + DOWNLOAD_DATA_TABLE + ";"""
 	create_table_query = """CREATE table """ + DOWNLOAD_DATA_TABLE + """(id serial, """
 
-	i=0
-	collen = len(columns)
-	insert_stmt = """\copy """ + DOWNLOAD_DATA_TABLE + " ("
+	insert_stmt = "\copy " + DOWNLOAD_DATA_TABLE + " ("
 
 	#Add an _ to column names with spaces in them to make them sql-safe
 	#concatenate columns to build sql statement to create table and bulk copy
+	i=0
+	collen = len(columns)
 	for c in columns:
         	colname = ""
 	        for ch in c:
@@ -100,45 +102,47 @@ def bulkInsert():
 	insert_stmt = """\copy input_data (CAMIS, DBA, BORO, BUILDING, STREET, ZIPCODE, PHONE, CUISINE_DESCRIPTION, INSPECTION_DATE, ACTION, VIOLATION_CODE, VIOLATION_DESCRIPTION, CRITICAL_FLAG, SCORE, GRADE, GRADE_DATE, RECORD_DATE, INSPECTION_TYPE) FROM /tmp/restaurantgrades2016-03-04.csv with CSV QUOTE '"'"""
 	print "Copy string is: " + insert_stmt + "|"
 
+	#Also prepare quriers to add primary key and run an analyze after bulk upload of data
 	add_pkey = "ALTER TABLE " + DOWNLOAD_DATA_TABLE + " ADD PRIMARY KEY (id);"
 	analyze_query = "ANALYZE " + DOWNLOAD_DATA_TABLE + ";"
 
 	print get_timestamp() + ": Now preparing to insert rows into " + DOWNLOAD_DATA_TABLE + "\n"
-	cur = conn.cursor()
-	try:
-		cur.execute(drop_table_query)
-	except:
-		print "Couldn't even drop table"
-
-	try:
-		cur.execute(create_table_query)
-	except:
-		print "Couldn't even create table"
-	try:
-		conn.commit()
-	except:
-		print "Can't commit dropping table and recreating it"
-
 	insert_cur = conn.cursor()
+
+	#Now perform queries to drop table, recreate, bulk copy, add primary key and run an analyze
+	try:
+		insert_cur.execute(drop_table_query)
+	except Exception as e:
+		print "Couldn't drop table"
+		return render_template("bulkInsertGradesResults.html", outcome="failed with error: " + str(e))
+
+	try:
+		insert_cur.execute(create_table_query)
+	except Exception as e:
+		print "Couldn't even create table"
+		return render_template("bulkInsertGradesResults.html", outcome="failed with error: " + str(e))
+
 
 	try:
 		#insert_cur.execute(insert_stmt + add_pkey + analyze_query)
 		insert_cur.execute(insert_stmt)
 	except Exception as e:
 		#print get_timestamp() + ": Unable to create table and bulk upload"
-		print get_timestamp() + ": Unable to bulk upload with error: " + str(e)
+		print get_timestamp() + ": Bulk insert failed"
+		return render_template("bulkInsertGradesResults.html", outcome="failed with error: " + str(e))
 
 	try:
 		insert_cur.execute(add_pkey)
-	except:
+	except Exception as e:
 		print get_timestamp() + ": Unable to add primary key"
+		return render_template("bulkInsertGradesResults.html", outcome="failed with error: " + str(e))
+
 
 	try:
 		insert_cur.execute(analyze_query)
-	except:
+	except Exception as e:
 		print get_timestamp() + ": Unable to analyze table"
-
-	return render_template("bulkInsertGrades.html")
+		return render_template("bulkInsertGradesResults.html", outcome="failed with error: " + str(e))
 
 
 	insert_cur.close()
@@ -149,8 +153,10 @@ def bulkInsert():
 	create_rest_query = "drop table if exists restaurant; create table restaurant as (select distinct camis, dba, boro, building, street, zipcode, phone, cuisine_description from " + DOWNLOAD_DATA_TABLE + "); alter table restaurant add primary key (camis);"
 	try:
 		rest_cur.execute(create_rest_query)
-	except:
+	except Exception as e:
 		print get_timestamp() + ": Could not create restaurant table"
+		return render_template("bulkInsertGradesResults.html", outcome="failed with error: " + str(e))
+
 
 	print get_timestamp() + ": Created restaurant table"
 	rest_cur.close()
@@ -162,8 +168,10 @@ def bulkInsert():
 	add_restgrades_pkey_query = "alter table restaurant_grades add primary key (camis, inspection_date, inspection_type, grade);"
 	try:
 		restgrades_cur.execute(drop_restgrades_query + create_restgrades_query + add_restgrades_pkey_query)
-	except:
+	except Exception as e:
 		print get_timestamp() + ": Could not create restaurant_grades table"
+		return render_template("bulkInsertGradesResults.html", outcome="failed with error: " + str(e))
+
 
 	print get_timestamp() + ": Created restaurant grades table"
 
@@ -171,4 +179,5 @@ def bulkInsert():
 	conn.commit()
 	conn.close()
 
+	return render_template("bulkInsertGrades.html", outcome="succeeded!")
 
