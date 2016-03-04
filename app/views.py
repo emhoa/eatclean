ae4bc6dddd845dbefd317c1f4b832e881ad97bba
Hoa Nguyen <emhoa@hotmail.com>
2016-03-04 13:44:53 -0500

    revision

--- app/views.py
+++ app/views.py
@@ -22,6 +22,25 @@ def get_timestamp():
 
 @app.route('/restaurants', methods=['POST'])
 def bulkInsert():
+	select_query = """select distinct dba, building, street, boro, phone, score::int, grade, grade_date from restaurant_grades rg, restaurant r where r.camis = rg.camis and r.cuisine_description = 'Thai' and r.camis not in (select camis from restaurant_grades where grade not in ('A', 'B')) order by grade_date desc, score::int asc limit 10;"""
+ 	#Connect to postgres database
 
-	return render_template("bulkInsertGrades.html", outcome="")
+       	urlparse.uses_netloc.append("postgres")
+        url = urlparse.urlparse(os.environ["DATABASE_U
+RL"])
+
+       	try:
+               	conn = psycopg2.connect(database=url.p
+ath[1:], user=url.username, password=url.password, hos
+t=url.hostname, port=url.port)
+        except Exception as e:
+       	        print "Unable to connect to the databa
+se"
+	cur = conn.cursor()
+	cur.execute(select_query)
+
+	mexican_eateries = ""
+	for (dba, building, street, boro, phone, score, grade, grade_date) in cursor:
+		mexican_eateris += "{}, {} {} in {} (tel: {}) (Grade: {} on {%b/%d/%Y} Score: {})\n".format(dba, building, street, boro, phone, grade, grade_date, score) 
+	return render_template("bulkInsertGradesResults.html", outcome=Markup(mexican_eateries))
 
